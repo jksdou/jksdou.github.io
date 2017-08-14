31fa7a441299464c99ac10aa19e7367ef4a7367a
唐钰豆豆 <doudoudzj@sina.com>
2017-08-15 04:22:49 +0800

    Static resource change to VPS

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $(window).load(function() {
-    $('.preloader').delay(1000).fadeOut("slow"); 
+    $('.preloader').delay(500).fadeOut("slow"); 
 });
 
 $(function() {
     jQuery(document).ready(function() {
         $('body').backstretch([
-          "images/background-0.jpg",
-          "images/background-1.jpg",
-          "images/background-2.jpg",
-          "images/background-3.jpg"
+          "//static.crogram.com/douzhenjiang/background-0.jpg",
+          "//static.crogram.com/douzhenjiang/background-1.jpg",
+          "//static.crogram.com/douzhenjiang/background-2.jpg",
+          "//static.crogram.com/douzhenjiang/background-3.jpg"
         ], { duration: 3200, fade: 1300 });
     });
 })
