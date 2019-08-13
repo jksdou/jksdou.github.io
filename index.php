6e9244e135d26d54acb9eb1b9e88e6155e3590f0
唐钰豆豆 <doudoudzj@gmail.com>
2019-08-13 10:43:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $suffix = pathinfo($_SERVER['HTTP_HOST'], PATHINFO_EXTENSION);
 if ($suffix == 'com') {
-  include 'index_en_US.html';
+  include 'index.html';
 } else {
   include 'index_zh_CN.html';
 }
